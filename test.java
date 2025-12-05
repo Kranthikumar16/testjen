0ed93a53940ade32907d4fc86af45e6fc2498ece
Kranthikumar16 <pentelakranthi2000@gmail.com>
2025-12-05 16:34:58 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello, man it is easy");
-        
+        System.out.println("new options");
     }
 }
