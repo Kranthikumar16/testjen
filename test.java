21a6c801933bbedeba99e1a3202e0145a042e54f
Kranthikumar16 <pentelakranthi2000@gmail.com>
2025-12-05 16:24:48 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("it is working fine");
+        System.out.println("Hello, man it is easy");
+        
     }
 }
